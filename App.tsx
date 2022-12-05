932c5ab48ea0a4188ad5d5a5d3e88162dba2bd63
Michel Klein <michel.kps@outlook.com>
2022-12-05 10:01:37 -0300

    feat: nativebase added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <NativeBaseProvider>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
       {fontsLoaded ? <Text>Hello World</Text> : <View />}
-    </View>
+    </NativeBaseProvider>
   );
 }
 
